d48dc8d21e9535240ed05999764169c2264f0ace
= <turlough.cowman@over-c.com>
2018-10-14 15:02:48 +0100

    Directory parameter for split_video.sh

--- split_video.sh
+++ split_video.sh
@@ -5,26 +5,39 @@ split_one(){
 	
 
 	filename=$1
+	sourcedir=$2
 	name=$(echo "$filename" | cut -f 1 -d '.')
 	clear
 	echo "___ $name ___"
 	echo
 
-	sourcefile="video/${filename}"
+	sourcefile="$sourcedir/${filename}"
 	destination="tf_files/training_set/$name"
 	mkdir $destination
 
 	echo "Splitting $sourcefile into $destination directory"
 
-	ffmpeg -i $sourcefile -r 4 $destination/$name%05d.jpg
+	# ffmpeg -i $sourcefile -r 4 $destination/$name%05d.jpg
 	echo
 	count="ls $destination | wc -l"
 	echo "$(eval $count) jpeg files added to $destination"
 }
 
-for file in $(eval ls video)
+
+if [ -z "$1" ]
+  then
+  	RED=`tput setaf 1`
+  	NC=`tput sgr0`
+    echo "${RED} You need to supply the path to a directory. ${NC}Usage: ./split_video <directory>"
+    exit
+fi
+dir=$1
+
+for file in $(eval ls $dir/*.mp4)
 do
-	split_one $file
+	filename=$(basename $file)
+	echo "$filename"
+	split_one $filename $dir
 done
 
 echo
